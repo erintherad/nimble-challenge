9fb78cbf282297331816eb346f7617f1ac95e039
Erin Mahoney <erinmahoneyphoto@gmail.com>
2017-09-22 11:44:40 -0700

    moves table in variable for readability

--- src/App.js
+++ src/App.js
@@ -9,15 +9,20 @@ var TableHeaderColumn = ReactBsTable.TableHeaderColumn;
 
 class App extends Component {
   render() {
+    const table = <BootstrapTable data={ userData } striped hover condensed>
+                  <TableHeaderColumn isKey dataField='name'>Applicant</TableHeaderColumn>
+                  <TableHeaderColumn dataField='status'>Status</TableHeaderColumn>
+                  <TableHeaderColumn dataField='applicationDate'>Application Date</TableHeaderColumn>
+                  <TableHeaderColumn dataField='location'>Location</TableHeaderColumn>
+                </BootstrapTable>
     return (
-      <div className="App">
-        <BootstrapTable data={userData} striped hover version='4'>
-          <TableHeaderColumn isKey dataField='id'>ID</TableHeaderColumn>
-          <TableHeaderColumn dataField='name'>Applicant</TableHeaderColumn>
-          <TableHeaderColumn dataField='status'>Status</TableHeaderColumn>
-          <TableHeaderColumn dataField='applicationDate'>Application Date</TableHeaderColumn>
-          <TableHeaderColumn dataField='location'>Location</TableHeaderColumn>
-        </BootstrapTable>
+      <div className="App container-fluid">
+        <div className="row table-row">
+          <div id="user-table">
+            <h2>Users</h2>
+            { table }
+          </div>
+        </div>
       </div>
     );
   }
