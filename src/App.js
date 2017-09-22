2c85f46a34e6933bfeff4aad210f916e38b98e96
Erin Mahoney <erinmahoneyphoto@gmail.com>
2017-09-22 14:57:08 -0700

    creates list group

--- src/App.js
+++ src/App.js
@@ -1,30 +1,35 @@
 import React, { Component } from 'react';
+import { ListGroup, ListGroupItem } from 'react-bootstrap';
 import './App.css';
 import UserModal from './modal.js';
-import { Button, Modal } from 'react-bootstrap';
 
 // User data
 var userData = require('./data.json');
-var ReactBsTable  = require('react-bootstrap-table');
-var BootstrapTable = ReactBsTable.BootstrapTable;
-var TableHeaderColumn = ReactBsTable.TableHeaderColumn;
 
 class App extends Component {
   render() {
-    const table = <BootstrapTable data={ userData } striped hover condensed>
-                  <TableHeaderColumn isKey dataField='name'>Applicant</TableHeaderColumn>
-                  <TableHeaderColumn dataField='status'>Status</TableHeaderColumn>
-                  <TableHeaderColumn dataField='applicationDate'>Application Date</TableHeaderColumn>
-                  <TableHeaderColumn dataField='location'>Location</TableHeaderColumn>
-                </BootstrapTable>
+    const userGroup = <ListGroup className="user-list">
+                    {
+                      userData.map(function(user) {
+                        return <ListGroupItem key={ user.id }>
+                                { user.name }
+                                <UserModal
+                                  name={ user.name }
+                                  status={ user.status }
+                                  applicationDate={ user.applicationDate }
+                                  location={ user.location}
+                                />
+                              </ListGroupItem>
+                      })
+                    }
+                  </ListGroup>
 
     return (
-      <div className="App container-fluid">
-        <div className="row table-row">
-          <div id="user-table">
-            <h2>Users</h2>
-            { table }
-            <UserModal />
+      <div className="container">
+        <div className="list-row row">
+          <div className="col-md-12 text-center">
+            <h2 >Users</h2>
+            { userGroup }
           </div>
         </div>
       </div>
