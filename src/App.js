ad070b7a3bd83138726a32739ac5b61897172c27
Erin Mahoney <erinmahoneyphoto@gmail.com>
2017-09-22 10:30:45 -0700

    adds react-table

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
-import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
-import 'node_modules/react-bootstrap-table/dist/react-bootstrap-table-all.min.css';
 
 // User data
 var userData = require('./data.json');
+var ReactBsTable  = require('react-bootstrap-table');
+var BootstrapTable = ReactBsTable.BootstrapTable;
+var TableHeaderColumn = ReactBsTable.TableHeaderColumn;
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h2>Users</h2>
+        <BootstrapTable data={userData} striped hover>
+          <TableHeaderColumn isKey dataField='id'>ID</TableHeaderColumn>
+          <TableHeaderColumn dataField='name'>Applicant</TableHeaderColumn>
+          <TableHeaderColumn dataField='status'>Status</TableHeaderColumn>
+        </BootstrapTable>
       </div>
     );
   }
