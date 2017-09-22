1ebaf1f2f8ac94db7fbb8fbabd8d20b7595f0b44
Erin Mahoney <erinmahoneyphoto@gmail.com>
2017-09-22 10:17:52 -0700

    adds react-table

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
+import 'node_modules/react-bootstrap-table/dist/react-bootstrap-table-all.min.css';
+
+// User data
+var userData = require('./data.json');
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h2>Hello World</h2>
+        <h2>Users</h2>
       </div>
     );
   }
