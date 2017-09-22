ecfdc3e9aeac77eee91301be043051a4522fed72
Erin Mahoney <erinmahoneyphoto@gmail.com>
2017-09-22 11:23:15 -0700

    adds table headers with data

--- src/App.js
+++ src/App.js
@@ -11,10 +11,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <BootstrapTable data={userData} striped hover>
+        <BootstrapTable data={userData} striped hover version='4'>
           <TableHeaderColumn isKey dataField='id'>ID</TableHeaderColumn>
           <TableHeaderColumn dataField='name'>Applicant</TableHeaderColumn>
           <TableHeaderColumn dataField='status'>Status</TableHeaderColumn>
+          <TableHeaderColumn dataField='applicationDate'>Application Date</TableHeaderColumn>
+          <TableHeaderColumn dataField='location'>Location</TableHeaderColumn>
         </BootstrapTable>
       </div>
     );
